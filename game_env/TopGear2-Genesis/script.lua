50cd5b5ac74663246336f2586efc464173d79caa
duke <duke.img@gmail.com>
2018-05-22 12:31:12 +0300

    Bugfix

--- game_env/TopGear2-Genesis/script.lua
+++ game_env/TopGear2-Genesis/script.lua
@@ -1,13 +1,19 @@
--- oldspeed = 0
+oldspeed = 0
 oldposition = 1
 function reward ()
   newreward = 0
   newspeed = data.speed
   newpostion = data.position
 
-  speedreward = (newspeed - 4000) / 2500.0
-
+  speedreward = newreward / 1000000.0
   newreward = newreward + speedreward
+
+  --Slowdown penalty
+  if oldspeed > 0 then
+    if (oldspeed - newspeed) / oldspeed > 0.3 then
+      newreward = newreward - 1000
+    end
+  end
 
   -- if car goes on the grass on the left side
   if data.side == 255 and data.pos < 35 or data.side == 254 then
@@ -22,7 +28,7 @@ function reward ()
   newreward = newreward + ((oldposition - newpostion) * 10000)
 
   oldposition = newpostion
-  -- oldspeed = newspeed
+  oldspeed = newspeed
 
   return newreward
 end
